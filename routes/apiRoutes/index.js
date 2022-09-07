d47ecaf56c532155bf6762e79bd547389daad6d5
cykj40 <102045473+cykj40@users.noreply.github.com>
2022-09-07 10:49:41 -0400

    Update index.js

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,18 +1,18 @@
 // dependencies
-const express  = require('express');
-const router = express.Router();
-const uuid = require('uuid');
-const Db = require('');
+import { Router } from 'express';
+const router = Router();
+import uuid from 'uuid';
+import { readNotes, addNote, deleteNote } from '';
 
 // set up /api/notes get route
 
 router.get('/api/notes', async function (_req, res) { 
-    const notes = await Db.readNotes();
+    const notes = await readNotes();
     return res.json(notes);
 });
 // set up /api/notes post route 
 router.post('/api/notes', async function (req, res)  {
-const currentNotes = await Db.readNotes();
+const currentNotes = await readNotes();
 
     let newNote = {
         id: uuid(),
@@ -20,20 +20,20 @@ const currentNotes = await Db.readNotes();
         text: req.body.text
  };
 
-await Db.addNote([...currentNotes, newNote]);
+await addNote([...currentNotes, newNote]);
 
 return res.send(newNote);
 });
 
 router.delete("/api/notes/:id", async function (req, res){
     const noteToDelete = req.params.id;
-    const currentNotes = await Db.readNotes();
+    const currentNotes = await readNotes();
     const newNoteData = currentNotes.filter((note) => note.id !== noteToDelete);
-    await Db.deleteNote(newNoteData);
+    await deleteNote(newNoteData);
     return res.send(newNoteData);
 });
 
-module.exports = router;
+export default router;
  
 
 
